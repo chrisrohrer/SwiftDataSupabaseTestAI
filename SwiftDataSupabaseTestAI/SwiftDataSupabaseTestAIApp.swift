da1357090eb1079a7ad5df9f28f2e942c05d77c3
Christoph Rohrer <christoph@rohrers.de>
2025-02-15 16:14:29 +0100

    Initial Commit

---
+++ SwiftDataSupabaseTestAI/SwiftDataSupabaseTestAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataSupabaseTestAIApp.swift
+//  SwiftDataSupabaseTestAI
+//
+//  Created by Christoph Rohrer on 15.02.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataSupabaseTestAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
